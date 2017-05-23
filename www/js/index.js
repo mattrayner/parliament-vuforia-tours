771dff5162ad137fe87f84bc2c0978e4acea2d6b
Matt Rayner <mattrayner1@gmail.com>
2017-05-23 21:55:25 +0100

    Initial Commit

---
+++ www/js/index.js
@@ -0,0 +1,188 @@
+var language = {
+    "en": {
+        "VUFORIA_MESSAGE": "Point your camera at a point of interest...",
+        "WELCOME_TITLE": "Welcome to Parliament Tours",
+        "WELCOME_BODY": "<p>You use this app by pointing your camera at a <strong>point of interest</strong>. Further information is then provided in your language of choice.</p><p>You can launch the camera by tapping the button in the header:</p><p><img src='img/camera-highlight.png' /></p>",
+        "HALL_TILE_TITLE": "Central Lobby Tiles",
+        "HALL_TILE_BODY": "<p>The Central Lobby tiles were originally designed by Augustus Pugin. They date back to xxxx and are currently undergoing a costly renovation.</p><p>After hundreds of years of footfall, many of the tiles are faded and their details obscured. In an effort to preserve them for future generations each tile is being painstakingly removed and restored.</p>",
+        "PAINTING_TITLE": "Charles I raising his standard at Nottingham",
+        "PAINTING_BODY": "<p>Nottingham, on 22 August 1642. On the left of the picture King Charles I, with his left arm upraised, his right around the shoulders of his twelve-year-old son, who would become Charles II. This was the event which signalled the start of the Civil War: the King set up his standard at Nottingham. The figure at the extreme right is Sir Edmund Verney, the Knight Marshal, who was in charge of the royal standard.</p><p>To the right, trumpeters blow a call, and the standard itself blows in the breeze, while in the foreground, soldiers and courtiers cheer. At the time the Royal Standard was set up, London and the Home Counties were already in the hands of the Parliamentary forces. It was considered a bad omen for the king's success that the Standard was blown down within a week.</p>",
+        "NEW-DAWN_TITLE": "New Dawn",
+        "NEW-DAWN_BODY": "<p>New Dawn is a contemporary sculpture by Mary Branson commemorating the long campaign that led to some women gaining the vote in 1918, and all women in 1928.</p><p>Made from metal and illuminated glass, New Dawn's design draws on concepts and imagery connected with Parliament and the votes for women movement.</p>",
+        "THRONE_TITLE": "The Royal Throne",
+        "THRONE_BODY": "<p>At the far end of the Chamber on a dais is the Royal Throne. This ornate gilded piece is based on the early 14th-century Coronation Chair in Westminster Abbey.</p><p>In front of the Throne is the Woolsack, on which the Lord Speaker sits. It is thought to have been introduced in the 14th century to reflect the economic importance of the wool trade to England. Over the years, its stuffing changed to hair, but in 1938 it was re-stuffed with wool from England, Wales, Scotland, Northern Ireland and countries of the Commonwealth, given by the International Wool Secretariat. The other two woolsacks are used by Judges at the State Opening of Parliament.</p>"
+    },
+    "fr": {
+        "VUFORIA_MESSAGE": "Pointez votre appareil photo en un point d'interet...",
+        "WELCOME_TITLE": "Bienvenue &#xE0; Parliament Tours",
+        "WELCOME_BODY": "<p>Vous utilisez cette application en pointant votre cam&#xE9;ra &#xE0; une <strong>point d&#x27;int&#xE9;r&#xEA;t</strong>. De plus amples informations sont fournies dans votre langue de choix.</p><p>Vous pouvez lancer l&#x27;appareil photo en appuyant sur le bouton dans l&#x27;en-t&#xEA;te:</p><p><img src='img/camera-highlight.png' /></p>",
+        "HALL_TILE_TITLE": "Carreaux de hall central",
+        "HALL_TILE_BODY": "<p>Les carreaux du Lobby central ont &#xE9;t&#xE9; con&#xE7;us &#xE0; l&#x27;origine par Augustus Pugin. Ils remontent &#xE0; xxxx et font actuellement l&#x27;objet d&#x27;une r&#xE9;novation co&#xFB;teuse.</p><p>Apr&#xE8;s des centaines d&#x27;ann&#xE9;es de footfall, beaucoup de carreaux sont fan&#xE9;s et leurs d&#xE9;tails obscurci. Dans un souci de les conserver pour les g&#xE9;n&#xE9;rations futures, chaque carreau est soigneusement retir&#xE9; et restaur&#xE9;.</p>",
+        "PAINTING_TITLE": "Charles I soul&#xE8;ve sa norme &#xE0; Nottingham",
+        "PAINTING_BODY": "<p>Nottingham, le 22 ao&#xFB;t 1642. Sur la gauche de la photo, le roi Charles Ier, le bras gauche crois&#xE9;, le droit autour des &#xE9;paules de son fils de douze ans, qui deviendrait Charles II. C&#x27;est l&#x27;&#xE9;v&#xE9;nement qui a marqu&#xE9; le d&#xE9;but de la guerre civile: le roi a &#xE9;tabli son niveau &#xE0; Nottingham. La figure &#xE0; l&#x27;extr&#xEA;me droite est Sir Edmund Verney, le chevalier-mar&#xE9;chal, qui &#xE9;tait responsable de la norme royale.</p><p>&#xC0; droite, les trompettes appellent, et la norme elle-m&#xEA;me souffle dans la brise, tandis qu&#x27;au premier plan, les soldats et les courtisans applaudissent. Au moment de la mise en place de la norme royale, Londres et les comt&#xE9;s d&#x27;origine &#xE9;taient d&#xE9;j&#xE0; entre les mains des forces parlementaires. Il a &#xE9;t&#xE9; consid&#xE9;r&#xE9; comme un mauvais augure pour le succ&#xE8;s du roi que le Standard a &#xE9;t&#xE9; d&#xE9;mantel&#xE9; dans une semaine.</p>",
+        "NEW-DAWN_TITLE": "Nouvelle aube",
+        "NEW-DAWN_BODY": "<p>New Dawn est une sculpture contemporaine de Mary Branson comm&#xE9;morant la longue campagne qui a men&#xE9; &#xE0; l&#x27;obtention d&#x27;un vote en 1918 et &#xE0; toutes les femmes en 1928.</p><p>Fabriqu&#xE9; en m&#xE9;tal et en verre illumin&#xE9;, le design de New Dawn s&#x27;appuie sur des concepts et des images li&#xE9;s au Parlement et les votes pour les mouvements f&#xE9;minins.</p>",
+        "THRONE_TITLE": "Le tr&#xF4;ne royal",
+        "THRONE_BODY": "<p>&#xC0; l&#x27;extr&#xE9;mit&#xE9; de la Chambre sur un estrade, se trouve le Tr&#xF4;ne royal. Cette pi&#xE8;ce dor&#xE9;e orn&#xE9;e est bas&#xE9;e sur la chaise de couronnement du d&#xE9;but du XIVe si&#xE8;cle dans l&#x27;abbaye de Westminster.</p><p>Devant le Tr&#xF4;ne se trouve le Woolsack, sur lequel se trouve le Lord Speaker. On pense qu&#x27;il a &#xE9;t&#xE9; introduit au 14&#xE8;me si&#xE8;cle pour refl&#xE9;ter l&#x27;importance &#xE9;conomique du commerce de la laine en Angleterre. Au fil des ann&#xE9;es, sa farce a chang&#xE9; en cheveux, mais en 1938, elle a &#xE9;t&#xE9; remplie de laine en Angleterre, au Pays de Galles, en Ecosse, en Irlande du Nord et dans les pays du Commonwealth, donn&#xE9;e par le Secr&#xE9;tariat international de la laine. Les deux autres laines sont utilis&#xE9;s par les juges lors de l&#x27;ouverture du Parlement.</p>"
+    },
+    "de": {
+        "VUFORIA_MESSAGE": "Zeigen Sie Ihre Kamera an einen Punkt des Interesses...",
+        "WELCOME_TITLE": "Willkommen bei Parliament Tours",
+        "WELCOME_BODY": "<p>Sie verwenden diese App, indem Sie Ihre Kamera auf eine <strong>Sehenswürdigkeit</strong>. Weitere Informationen finden Sie in Ihrer Sprache.</p><p>Sie können die Kamera starten, indem Sie auf die Schaltfläche im Header tippen:</p><p><img src='img/camera-highlight.png' /></p>",
+        "HALL_TILE_TITLE": "Zentrale Lobby Fliesen",
+        "HALL_TILE_BODY": "<p>Die zentralen Lobby Fliesen wurden ursprünglich von Augustus Pugin entworfen. Sie stammen aus xxxx und befinden sich derzeit in einer kostspieligen Renovierung.</p><p>After hundreds of years of footfall, many of the tiles are faded and their details obscured. In an effort to preserve them for future generations each tile is being painstakingly removed and restored.</p>",
+        "PAINTING_TITLE": "Charles Ich hebe seinen Standard in Nottingham",
+        "PAINTING_BODY": "<p>Nottingham, am 22. August 1642. Auf der linken Seite des Bildes König Karl I., mit seinem linken Arm erhoben, sein Recht um die Schultern seines zwölfjährigen Sohnes, der Karl II werden würde. Dies war die Veranstaltung, die den Beginn des Bürgerkriegs signalisierte: Der König stellte seinen Standard in Nottingham auf. Die Gestalt ganz rechts ist Sir Edmund Verney, der Rittermarschall, der für den königlichen Standard verantwortlich war.</p><p>Auf der rechten Seite blasen Trompeter einen Anruf, und der Standard selbst bläst in der Brise, während im Vordergrund Soldaten und Höflinge jubeln. Zu der Zeit, als der Königliche Standard eingerichtet wurde, waren London und die Heimatkreise bereits in den Händen der parlamentarischen Streitkräfte. Es wurde als ein schlechtes Omen für den Erfolg des Königs angesehen, dass der Standard innerhalb einer Woche geblasen wurde.</p>",
+        "NEW-DAWN_TITLE": "Neuanfang",
+        "NEW-DAWN_BODY": "<p>New Dawn ist eine zeitgenössische Skulptur von Mary Branson zum Gedenken an die lange Kampagne, die dazu führte, dass einige Frauen die Abstimmung im Jahr 1918 und alle Frauen im Jahr 1928.</p><p>Aus Metall und beleuchtetem Glas gefertigt, greift das Design von New Dawn auf Konzepte und Bilder, die mit dem Parlament verbunden sind, und die Stimmen für die Frauenbewegung.</p>",
+        "THRONE_TITLE": "Der Königliche Thron",
+        "THRONE_BODY": "<p>Am fernen Ende der Kammer auf einem Dais ist der Königliche Thron. Dieses aufwändige vergoldete Stück basiert auf dem frühen 14. Jahrhundert Krönungsstuhl in Westminster Abbey.</p><p>Vor dem Thron ist der Woolsack, auf dem der Lord Speaker sitzt. Es wird angenommen, dass es im 14. Jahrhundert eingeführt wurde, um die wirtschaftliche Bedeutung des Wollhandels nach England zu reflektieren. Im Laufe der Jahre veränderte sich seine Füllung in die Haare, aber 1938 wurde es mit Wolle aus England, Wales, Schottland, Nordirland und Ländern des Commonwealth, die vom Internationalen Wollsekretariat gegeben wurden, wieder gefüllt. Die beiden anderen Wollsachen werden von den Richtern bei der Staatsöffnung des Parlaments verwendet.</p>"
+    }
+};
+
+var t;
+var vuforiaOptions = {
+    databaseXmlFile: 'www/targets/ParliamentTest.xml',
+    targetList: [ 'hall_tile', 'throne', 'painting', 'painting2', 'painting3', 'new-dawn' ],
+    overlayMessage: 'language specific message goes here',
+    vuforiaLicense: 'ARWew8T/////AAAAAFfGjmmevkaStzS/ubwzoq41fZDzeg7vVS4hFrktpLuHbBXUVEe7yawMjAXfruf810aenI4bFAH4pHgM/D5ErIyLhsn6ct1qnMRGJy2tqcRsTOHZuqnEMRLiCwtnANvO8qxN5DXztFJSqCgrW2can9708d5o32QvsB/T6eD1BKao9ZIMXApddusf7NLizCHkxAESC7+UQVPZYeiwr1VoJqbyhvaxd1CdDo55/wfGgkuQQenFsRrAnppQcPyYn0C51GmgdCa8JY6ynp6jI5UWMC7TIIji64rO5PSkp3Tn0lRsifD+2MyOCm3jAhrpF42wPaWa/XG3qbXUvXlatcDjVJDM4+prw3U2iOl+xW2cdvBX'
+};
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+
+        var homeButton = document.getElementById('home');
+        homeButton.addEventListener('click', app.setupWelcome);
+
+        var launchVuforia = document.getElementById('vuforiaLaunch');
+        launchVuforia.addEventListener('click', app.launchVuforia);
+        console.log(launchVuforia);
+
+        var enButton = document.querySelector('#welcome .language.en');
+        enButton.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            t = language['en'];
+            app.setupContent('welcome', 'WELCOME');
+        });
+
+        var frButton = document.querySelector('#welcome .language.fr');
+        frButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            t = language['fr'];
+            app.setupContent('welcome', 'WELCOME');
+        });
+
+        var deButton = document.querySelector('#welcome .language.de');
+        deButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            t = language['de'];
+            app.setupContent('welcome', 'WELCOME');
+        });
+
+        app.setupWelcome();
+    },
+
+    setupWelcome: function(e) {
+        if(e) { e.preventDefault(); }
+
+        console.log('Setup Welcome Page');
+
+        // Hide the
+        var vuforiaButton = document.querySelector('.header .vuforia-launch');
+        vuforiaButton.style.display = 'none';
+
+        var welcomPage = document.getElementById('welcome');
+        welcomPage.style.display = 'block';
+
+        var contentPage = document.getElementById('content');
+        contentPage.style.display = 'none';
+    },
+
+    setupContent: function(imageName, translationPrefix) {
+        console.log('Setup Content Page: ' + imageName + " - " + translationPrefix);
+
+        // Update vuforia overlay message
+        vuforiaOptions.overlayMessage = t['VUFORIA_MESSAGE'];
+
+        var vuforiaButton = document.querySelector('.header .vuforia-launch');
+        vuforiaButton.style.display = 'block';
+
+        var welcomPage = document.getElementById('welcome');
+        welcomPage.style.display = 'none';
+
+        var contentPage = document.getElementById('content');
+        contentPage.style.display = 'block';
+
+        var contentImage = document.getElementById('image');
+        var contentTitle = document.getElementById('title');
+        var contentBody = document.getElementById('body');
+
+        contentImage.setAttribute('src', 'img/page/'+imageName+'.png');
+        contentTitle.innerHTML = t[translationPrefix+'_TITLE'];
+        contentBody.innerHTML = t[translationPrefix+'_BODY'];
+    },
+
+    launchVuforia: function(e) {
+        e.preventDefault();
+
+        console.log('Launch Vuforia');
+
+        navigator.VuforiaPlugin.startVuforia(
+            vuforiaOptions,
+            function(data) {
+                // To see exactly what `data` can return, see 'Success callback `data` API' within the plugin's documentation.
+                console.log(data);
+
+                if(data.status.imageFound) {
+                    var image = data.result.imageName;
+
+                    if(image.indexOf('painting') >= 0){ image = 'painting' }
+
+                    app.setupContent(image, image.toUpperCase());
+                }
+                else if (data.status.manuallyClosed) {
+                    alert("User manually closed Vuforia by pressing back!");
+                }
+            },
+            function(data) {
+                alert("Error: " + data);
+            }
+        );
+    }
+};
+
+app.initialize();
